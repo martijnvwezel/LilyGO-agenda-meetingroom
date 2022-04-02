0c53f4470dac333d470dd5497cdcd58348659bb4
martijn <martijnvwezel@muino.nl>
2022-04-02 09:27:07 +0200

    Adding keys

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # LilyGo-EPD-4-7-OWM-Weather-Display
+The LilyGO is an 4.7" e-paper screen. For this project the ESP32 will be located infront of a meetingroom.
+Currently, Open Weather Map is being used to show the weather on the LilyGo EPD 4.7" display.
 
-Open Weather Map weather station using LilyGo EPD 4.7" display
 
 
 # Compiling and flashing
 
 Edit `owm_credentials.h` and enter OWM API key as well as the location for which you want to display the weather data
 
-You need Platformio to compile
+You need Platformio to compile (on VScode)
---
+++ src/owm_credentials.h
@@ -0,0 +1,42 @@
+
+const bool DebugDisplayUpdate = false;
+
+const char* ssid     = "Muino";
+const char* password = "muinomuino";
+
+String internal_server = "http://000.000.000.000";
+
+// Use your own API key by signing up for a free developer account at https://openweathermap.org/
+String     apikey   = "your-api-key";
+const char server[] = "api.openweathermap.org";
+
+// Set your location according to OWM locations
+String City      = "Amsterdam";                              // Your home city See: http://bulk.openweathermap.org/sample/
+String Latitude  = "00.000";                                 // Latitude of your location in decimal degrees
+String Longitude = "0.0000";                                 // Longitude of your location in decimal degrees
+String Language  = "EN";                                     // NOTE: Only the weather description is translated by OWM
+                                                             // Examples: Arabic (AR) Czech (CZ) English (EN) Greek (EL) Persian(Farsi) (FA) Galician (GL) Hungarian (HU) Japanese (JA)
+                                                             // Korean (KR) Latvian (LA) Lithuanian (LT) Macedonian (MK) Slovak (SK) Slovenian (SL) Vietnamese (VI)
+String      Hemisphere = "north";                            // or "south"
+String      Units      = "M";                                // Use 'M' for Metric or I for Imperial
+const char* Timezone   = "MET-1METDST,M3.5.0/01,M10.5.0/02"; // Choose your time zone from: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+                                                             // See below for examples
+const char* ntpServer = "0.nl.pool.ntp.org";                 // Or, choose a time server close to you, but in most cases it's best to use pool.ntp.org to find an NTP server
+                                                             // then the NTP system decides e.g. 0.pool.ntp.org, 1.pool.ntp.org as the NTP syem tries to find  the closest available servers
+                                                             // EU "0.europe.pool.ntp.org"
+                                                             // US "0.north-america.pool.ntp.org"
+                                                             // See: https://www.ntppool.org/en/
+int gmtOffset_sec      = 3600;                               // UK normal time is GMT, so GMT Offset is 0, for US (-5Hrs) is typically -18000, AU is typically (+8hrs) 28800
+int daylightOffset_sec = 7200;                               // In the UK DST is +1hr or 3600-secs, other countries may use 2hrs 7200 or 30-mins 1800 or 5.5hrs 19800 Ahead of GMT use + offset behind - offset
+#define MAX_FOR_CASTS 5
+
+// * Example time zones
+// const char* Timezone = "MET-1METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
+// const char* Timezone = "CET-1CEST,M3.5.0,M10.5.0/3";       // Central Europe
+// const char* Timezone = "EST-2METDST,M3.5.0/01,M10.5.0/02"; // Most of Europe
+// const char* Timezone = "EST5EDT,M3.2.0,M11.1.0";           // EST USA
+// const char* Timezone = "CST6CDT,M3.2.0,M11.1.0";           // CST USA
+// const char* Timezone = "MST7MDT,M4.1.0,M10.5.0";           // MST USA
+// const char* Timezone = "NZST-12NZDT,M9.5.0,M4.1.0/3";      // Auckland
+// const char* Timezone = "EET-2EEST,M3.5.5/0,M10.5.5/0";     // Asia
+// const char* Timezone = "ACST-9:30ACDT,M10.1.0,M4.1.0/3":   // Australia
